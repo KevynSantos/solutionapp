68429d3a44d29b3905188fdb77f45d5436f4f3d5
KevynSantos <52923238+KevynSantos@users.noreply.github.com>
2025-06-06 21:05:14 -0300

    Adicionando permissões de CORS ao projeto de front-end

---
+++ src/main/java/com/solutionapp/solutionapp/configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package com.solutionapp.solutionapp.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+            .allowedOrigins(host)
+            .allowedMethods("*");
+  }
+  
+  @Value("${cors.access.front.end}")
+  private String host;
+}
