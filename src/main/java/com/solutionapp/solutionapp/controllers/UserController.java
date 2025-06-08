54aec1de1b0156825bc187f306c9c7e8d16971ed
KevynSantos <52923238+KevynSantos@users.noreply.github.com>
2025-06-07 22:46:47 -0300

    Alterando tipo de protocolo na rota addOrUpdate

--- src/main/java/com/solutionapp/solutionapp/controllers/UserController.java
+++ src/main/java/com/solutionapp/solutionapp/controllers/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,7 +31,7 @@ public class UserController extends AbstractController {
         return convertToJson(status, HttpStatus.OK);
     }
     
-    @PostMapping("/addOrUpdate")
+    @PutMapping("/addOrUpdate")
     public ResponseEntity<?> addOrUpdate(@RequestBody UserDto userDto)
     {
         Pair<HttpStatus, HashMap<String, Object>> res = this.userService.addOrUpdate(userDto);
